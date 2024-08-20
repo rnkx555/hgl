4a08b691af5856cc0bf6b6ea93bed914b00b2569
rnkx555 <p22013872@student.newinti.edu.my>
2024-08-20 22:06:47 +0800

    Add files via upload

---
+++ Happygolucky.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.happygolucky;
+
+/**
+ *
+ * @author rache
+ */
+public class Happygolucky {
+
+    public static void main(String[] args) {
+        System.out.println("Happy Go Lucky");
+    }
+}
